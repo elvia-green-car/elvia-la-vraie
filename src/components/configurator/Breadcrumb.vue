a1d228e40a747b085fc92f73253b45adb86d51e2
Maëva <maeva@nother.io>
2022-05-20 12:13:09 +0200

    update label from step Breadcrumb.vue

--- src/components/configurator/Breadcrumb.vue
+++ src/components/configurator/Breadcrumb.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="flex flex-col">
-    <div class="flex justify-end items-center gap-3" v-for="(step, index) in store.steps" :key="index" @click="stepClicked($event, step, index)">
-      <span :class="[step === activeStep ? 'capitalize' : 'hidden']">0{{ index + 1 }} - {{ step }}</span>
-      <span :class="[step === activeStep ? 'h-[10px]' : 'h-[2px] my-2', 'hover:h-[7px] transition-all ease-in-out w-16 bg-white']"/>
+    <div class="flex justify-end items-center gap-3" v-for="(step, index) in store.steps" :key="index"
+         @click="stepClicked($event, step, index)">
+      <span :class="[step === activeStep ? 'capitalize' : 'hidden']">0{{ index + 1 }} - {{ getLabel(step) }}</span>
+      <span
+          :class="[step === activeStep ? 'h-[10px]' : 'h-[2px] my-2', 'hover:h-[7px] transition-all ease-in-out w-16 bg-white']"/>
     </div>
   </div>
 </template>
@@ -22,9 +24,30 @@ export default {
   props: {
     activeStep: String,
   },
-  methods:{
+  methods: {
     stepClicked($e, step, index) {
       this.$emit('stepSelected', index)
+    },
+    getLabel(step) {
+      switch (step) {
+        case "Hood":
+          return "Capot"
+          break
+        case "Roof":
+          return "Toit"
+          break
+        case "Door":
+          return "Portières"
+          break
+        case "Trunk":
+          return "Coffre"
+          break
+        case "Global":
+          return "Vue globale"
+          break
+        default:
+          break
+      }
     }
   }
 }
